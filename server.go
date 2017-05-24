870037fa2170a51f70af4a2cc0f8dc34c4c6b04c
Asami Doi <doiasami1219@gmail.com>
2017-05-24 11:22:49 +0900

    add: sqlite3 database

--- server.go
+++ server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	_ "github.com/mattn/go-sqlite3"
 	"net/http"
 )
 
@@ -9,7 +12,15 @@ func getRoot(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+}
+
 func main() {
+	db := initDB("storage.db")
+	migrate(db)
+
 	e := echo.New()
 	e.GET("/", getRoot)
 	e.Logger.Fatal(e.Start(":1323"))
