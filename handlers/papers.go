f694af517df9c54f79b1bdacf706f1969d3db0dc
Asami Doi <doiasami1219@gmail.com>
2017-05-24 13:25:45 +0900

    modify: handler function

--- handlers/papers.go
+++ handlers/papers.go
@@ -7,3 +7,28 @@ import (
 
 	"github.com/labstack/echo"
 )
+
+type H map[string]interface{}
+
+func GetPapers(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "papers")
+	}
+}
+
+func PutPaper(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusCreated, H{
+			"created": 123,
+		})
+	}
+}
+
+func DeletePaper(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
+		return c.JSON(http.StatusOK, H{
+			"deleted": id,
+		})
+	}
+}
