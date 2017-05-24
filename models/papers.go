8172c3528d501a954a5fa8a7c723a28327dba465
Asami Doi <doiasami1219@gmail.com>
2017-05-24 15:05:35 +0900

    modify: getPapers function in models

--- models/papers.go
+++ models/papers.go
@@ -5,3 +5,36 @@ import (
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+type Paper struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type PaperCollection struct {
+	Papers []Paper `json:"items"`
+}
+
+func GetPapers(db *sql.DB) PaperCollection {
+	sql := "SELECT * FROM papers"
+	rows, err := db.Query(sql)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	result := PaperCollection{}
+	for row.Next() {
+		paper := Paper{}
+		err2 := rows.Scan(&paper.Id, &paper.Name)
+
+		if err2 != nil {
+			panic(err2)
+		}
+
+		result.Papers = append(result.Papers, paper)
+	}
+	return result
+}
