ab103b99f338d14946e4c29b6e8e791a2f5d48a8
Asami Doi <doiasami1219@gmail.com>
2017-05-24 09:32:11 +0900

    modify: learn channels

--- hello.go
+++ hello.go
@@ -43,7 +43,24 @@ func say(s string) {
 	}
 }
 
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
 func main() {
+	s := []int{7, 2, 8, -9, 4, 0}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c
+
+	fmt.Println(x, y, x+y)
+
 	go say("world")
 	say("hello")
 
